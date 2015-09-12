29db817b17ba50c33656b9ea1403b5ae38b8b80d
majpage <w.maj@majpage.com>
2015-09-12 12:58:47 +0200

    Change htaccess and parameters file, add repositories and db tables' configuration.

---
+++ src/AppBundle/Entity/Repository/PilgrimApplicationRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Repository
+ */
+class PilgrimApplicationRepository extends EntityRepository
+{
+}
---
+++ src/AppBundle/Entity/Repository/ScoutApplicationRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Repository
+ */
+class ScoutApplicationRepository extends EntityRepository
+{
+}
