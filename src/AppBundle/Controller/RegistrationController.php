bb041ef6487abe2bb753852fcf980145356dac69
majpage <w.maj@majpage.com>
2015-09-12 18:21:59 +0200

    Add forms and "success" page.

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -2,7 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\PilgrimApplicationType;
+use AppBundle\Form\Type\ScoutApplicationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Controller
@@ -22,20 +26,66 @@ class RegistrationController extends Controller
     /**
      * Pilgrims action
      *
+     * @param Request $request request
+     *
      * @return Response
      */
-    public function pilgrimsAction()
+    public function pilgrimsAction(Request $request)
     {
-        return $this->render('AppBundle::registration/pilgrims.html.twig');
+        $form = $this->createForm(new PilgrimApplicationType(), null, array(
+            'action' => $this->generateUrl('registration_pilgrims'),
+            'method' => 'POST',
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // @TODO: Add form data saving
+            $response = $this->redirect($this->generateUrl('registration_success'));
+        } else {
+            $response = $this->render('AppBundle::registration/pilgrims.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+
+        return $response;
     }
 
     /**
      * Scouts action
      *
+     * @param Request $request request
+     *
      * @return Response
      */
-    public function scoutsAction()
+    public function scoutsAction(Request $request)
     {
-        return $this->render('AppBundle::registration/scouts.html.twig');
+        $form = $this->createForm(new ScoutApplicationType(), null, array(
+            'action' => $this->generateUrl('registration_pilgrims'),
+            'method' => 'POST',
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // @TODO: Add form data saving
+            $response = $this->redirect($this->generateUrl('registration_success'));
+        } else {
+            $response = $this->render('AppBundle::registration/scouts.html.twig', array(
+                'form' => $form->createView(),
+            ));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Success action
+     *
+     * @return Response
+     */
+    public function successAction()
+    {
+        return $this->render('AppBundle::registration/success.html.twig');
     }
 }
