e047830cc884c5e7da21c0dc2557c5bd9dae9a8c
majpage <w.maj@majpage.com>
2016-01-04 02:20:21 +0100

    Add full regions/districts support and JS support for regions/districts and services.

--- public_html/js/registration/main.js
+++ public_html/js/registration/main.js
@@ -1,8 +1,12 @@
 ;(function ($, document, undefinedType) {
 
-    var fieldsChange = function () {
-        var form = $('.registration-form'),
-            allElements = form.find('[data-specific-locale]');
+    /**
+     * Hide specific fields
+     *
+     * @param {jQuery} form form
+     */
+    var hideSpecificFields = function (form) {
+        var allElements = form.find('[data-specific-locale]');
         if (allElements.length === 0) {
             return;
         }
@@ -38,10 +42,79 @@
         changeLocale();
     };
 
-    $(document).ready(function() {
-        fieldsChange();
+    /**
+     * Bind districts with regions
+     *
+     * @param {jQuery} form form
+     */
+    var bindDistrictsWithRegions = function (form) {
+        var regionsField = form.find('[id$="_regionId"]').first(),
+            districtsField = form.find('[id$="_districtId"]').first();
+        if (regionsField.length !== 1 || districtsField.length !== 1) {
+            return;
+        }
+        var allDistricts = districtsField.find('option'),
+            districts = {};
 
-        $('.registration-form [data-toggle="tooltip"]').tooltip();
+        districtsField.find('option').each(function () {
+            var regionId = parseInt(Math.floor(parseInt($(this).val(), 10) / 1000), 10);
+            if (typeof districts[regionId] === undefinedType) {
+                districts[regionId] = $(this);
+            } else {
+                districts[regionId] = districts[regionId].add($(this));
+            }
+        });
+
+        var changeDistricts = function () {
+            var regionId = parseInt(regionsField.val(), 10);
+            allDistricts.hide();
+            if (typeof districts[regionId] !== undefinedType) {
+                districts[regionId].show();
+            }
+            allDistricts.filter(':visible:first').prop('selected', true);
+        };
+        
+        regionsField.on('change', changeDistricts);
+        changeDistricts();
+    };
+
+    /**
+     * Manage services
+     *
+     * @param {jQuery} form form
+     */
+    var manageServices = function (form) {
+        var serviceMainField = form.find('[id$="_serviceMainId"]:first'),
+            serviceMainOptions = serviceMainField.find('option'),
+            serviceExtraOptions = form.find('[id$="_serviceExtraId"]:first option');
+        if (serviceMainField.length !== 1 || serviceMainOptions.length < 1 || serviceExtraOptions.length < 1) {
+            return;
+        }
+
+        var diversifyServices = function () {
+            var index = serviceMainOptions.filter(':selected').val(),
+                toHide = serviceExtraOptions.filter('[value="' + index + '"]');
+            serviceExtraOptions.show();
+            toHide.hide();
+
+            if (toHide.is(':selected')) {
+                toHide.prop('selected', false);
+                serviceExtraOptions.filter(':visible:first').prop('selected', true);
+            }
+        };
+
+        serviceMainField.on('change', diversifyServices);
+        diversifyServices();
+    };
+
+    $(document).ready(function() {
+        var form = $('.registration-form').first();
+        if (form.length === 1) {
+            hideSpecificFields(form);
+            bindDistrictsWithRegions(form);
+            manageServices(form);
+            form.find('[data-toggle="tooltip"]').tooltip();
+        }
     });
 
 })(jQuery, document, 'undefined');
