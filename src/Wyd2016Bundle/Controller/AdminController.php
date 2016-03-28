71732852873cdbc599d5384377cdd45c27a2baa3
majpage <w.maj@majpage.com>
2016-03-28 05:16:26 +0200

    Change link on widget's titles.

--- src/Wyd2016Bundle/Controller/AdminController.php
+++ src/Wyd2016Bundle/Controller/AdminController.php
@@ -26,32 +26,36 @@ class AdminController extends Controller
             'limit' => $limit,
             'lists' => array(
                 'volunteers' => array(
+                    'counter' => 'admin.volunteers.counter',
                     'items' => $this->get('wyd2016bundle.volunteer.repository')
                         ->findBy($criteria, $orderBy, $limit),
                     'routeIndex' => 'admin_volunteer_index',
                     'routeShow' => 'admin_volunteer_show',
-                    'title' => 'admin.volunteers.widget',
+                    'title' => 'admin.volunteers',
                 ),
                 'troops' => array(
+                    'counter' => 'admin.troops.counter',
                     'items' => $this->get('wyd2016bundle.troop.repository')
                         ->findBy($criteria, $orderBy, $limit),
                     'routeIndex' => 'admin_troop_index',
                     'routeShow' => 'admin_troop_show',
-                    'title' => 'admin.troops.widget',
+                    'title' => 'admin.troops',
                 ),
                 'pilgrims' => array(
+                    'counter' => 'admin.pilgrims.counter',
                     'items' => $this->get('wyd2016bundle.pilgrim.repository')
                         ->findBy($criteria, $orderBy, $limit),
                     'routeIndex' => 'admin_pilgrim_index',
                     'routeShow' => 'admin_pilgrim_show',
-                    'title' => 'admin.pilgrims.widget',
+                    'title' => 'admin.pilgrims',
                 ),
                 'groups' => array(
+                    'counter' => 'admin.groups.counter',
                     'items' => $this->get('wyd2016bundle.group.repository')
                         ->findBy($criteria, $orderBy, $limit),
                     'routeIndex' => 'admin_group_index',
                     'routeShow' => 'admin_group_show',
-                    'title' => 'admin.groups.widget',
+                    'title' => 'admin.groups',
                 ),
             ),
         ));
